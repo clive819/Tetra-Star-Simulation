c40af4915ac4b1e6767c08a9667050053951f837
Clive <clive819@gmail.com>
2021-05-30 20:43:21 -0700

    update TLogger

--- src/main/java/logging/TLogger.java
+++ src/main/java/logging/TLogger.java
@@ -1,27 +1,26 @@
 package main.java.logging;
 
 import javax.swing.*;
-import java.io.OutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class TLogger {
 
     public static TLogger shared = new TLogger();
 
-    private final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private JTextArea textArea;
 
     private TLogger() {
     }
 
     public void log(String message) {
-        logger.log(Level.INFO, message);
+        if (textArea != null) {
+            textArea.append(message + "\n");
+        } else {
+            System.out.println(message);
+        }
     }
 
     public void setOutput(JTextArea textArea) {
-        logger.setUseParentHandlers(false);
-        OutputStream os = new TextAreaOutputStream(textArea);
-        logger.addHandler(new TextAreaHandler(os));
+        this.textArea = textArea;
     }
 
 }
